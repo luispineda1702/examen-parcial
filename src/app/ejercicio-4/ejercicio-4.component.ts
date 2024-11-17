4fb220d1ea20d1e30d2be1616b3410899543db6f
luispineda1702 <ln72484067@gmail.com>
2024-11-17 11:20:50 -0500

    CUarto ejercicio resuelto

--- src/app/ejercicio-4/ejercicio-4.component.ts
+++ src/app/ejercicio-4/ejercicio-4.component.ts
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class Ejercicio4Component {
 
+  pares: number[] = []; 
+  mostrarPares() { 
+    this.pares = []; 
+    for (let i = 20; i >= 10; i--) { 
+      if (i % 2 === 0) { 
+        this.pares.push(i); 
+      } 
+    } 
+  }
 }
