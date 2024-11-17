4669a00bff7fd5df06d742a6245ebc509d0f4394
luispineda1702 <ln72484067@gmail.com>
2024-11-17 10:59:34 -0500

    Segundo ejercicio resuelto

--- src/app/ejercicio-2/ejercicio-2.component.ts
+++ src/app/ejercicio-2/ejercicio-2.component.ts
@@ -9,17 +9,58 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './ejercicio-2.component.css'
 })
 export class Ejercicio2Component {
+  
+  simulacion = ""
   formPrest={
     prest:""
   }
 
   calcularPrestamo(){
-    var cuotas:null
-    var valorCuota:null
-    var interes:null
-    var interesFinal:null
+    var cuotas = null
+    var valorCuota = null
+    var interes = null
+    var interesFinal = null
+    
 
     let prestamo = Number(this.formPrest.prest)
-
+    if(prestamo<=4000){
+      interesFinal = prestamo * 0.12
+      if(prestamo>5000){
+        cuotas=3
+        valorCuota=interesFinal/cuotas
+      }
+      else if(prestamo<1000){
+        cuotas=1
+        valorCuota=interesFinal/cuotas
+      }
+      else if (prestamo >= 2000 && prestamo <= 3000) { 
+        cuotas = 2;
+        valorCuota = interesFinal / cuotas;
+      }
+      else{
+        cuotas=5
+        valorCuota=interesFinal/cuotas
+      }
+    }
+    else{
+      interesFinal = prestamo * 0.1
+      if(prestamo>5000){
+        cuotas=3
+        valorCuota=interesFinal/cuotas
+      }
+      else if(prestamo<1000){
+        cuotas=1
+        valorCuota=interesFinal/cuotas
+      }
+      else if (prestamo >= 2000 && prestamo <= 3000) { 
+        cuotas = 2;
+        valorCuota = interesFinal / cuotas;
+      }
+      else{
+        cuotas=5
+        valorCuota=interesFinal/cuotas
+      }
+    }
+    this.simulacion = `El total de cuotas a pagar es ${cuotas} y su valor es ${valorCuota}`
   }
 }
