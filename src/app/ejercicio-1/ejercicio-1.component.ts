b975b3394c8eb4ec70a7444bdf19d0b58cd6d37e
luispineda1702 <ln72484067@gmail.com>
2024-11-17 01:00:49 -0500

    Primer Commit

---
+++ src/app/ejercicio-1/ejercicio-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio-1.component.html',
+  styleUrl: './ejercicio-1.component.css'
+})
+export class Ejercicio1Component {
+
+}
