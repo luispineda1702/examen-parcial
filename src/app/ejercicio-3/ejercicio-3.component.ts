f52d67a9bf128f638677ca1108e034c261882076
luispineda1702 <ln72484067@gmail.com>
2024-11-17 11:14:08 -0500

    Tercer ejercicio resuelto

--- src/app/ejercicio-3/ejercicio-3.component.ts
+++ src/app/ejercicio-3/ejercicio-3.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio-3',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ejercicio-3.component.html',
   styleUrl: './ejercicio-3.component.css'
 })
 export class Ejercicio3Component {
 
+  dniFinal=""
+  formDni={
+    anio:null,
+  }
+
+  calcularDNI(){
+    let anioDNI = Number(this.formDni.anio)
+    let anioactual = 2024 - anioDNI
+    var diagnostico=""
+
+    if(anioactual>17){
+      diagnostico="DEBE REGISTRAR DNI"
+    }
+    else{
+      diagnostico="AUN NO DEBE REGISTRAR DNI"
+    }
+    this.dniFinal = `${diagnostico}`
+  }
 }
